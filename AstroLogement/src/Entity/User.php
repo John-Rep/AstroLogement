885175c3482b8258a1a7d0aa45d82e656ef8155b
YukioKamii <yoyolc.2004@gmail.com>
2025-03-26 17:15:39 +0100

    Création de l'entité Avis

--- AstroLogement/src/Entity/User.php
+++ AstroLogement/src/Entity/User.php
@@ -56,10 +56,17 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     #[ORM\OneToMany(targetEntity: Message::class, mappedBy: 'userDest', orphanRemoval: true)]
     private Collection $messagesRecus;
 
+    /**
+     * @var Collection<int, Avis>
+     */
+    #[ORM\OneToMany(targetEntity: Avis::class, mappedBy: 'user')]
+    private Collection $avis;
+
     public function __construct()
     {
         $this->locations = new ArrayCollection();
         $this->messagesRecus = new ArrayCollection();
+        $this->avis = new ArrayCollection();
     }
 
     public function getId(): ?int
@@ -235,5 +242,35 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
 {
     return $this->prenom . ' ' . $this->nom;
 }
+
+    /**
+     * @return Collection<int, Avis>
+     */
+    public function getAvis(): Collection
+    {
+        return $this->avis;
+    }
+
+    public function addAvi(Avis $avi): static
+    {
+        if (!$this->avis->contains($avi)) {
+            $this->avis->add($avi);
+            $avi->setUser($this);
+        }
+
+        return $this;
+    }
+
+    public function removeAvi(Avis $avi): static
+    {
+        if ($this->avis->removeElement($avi)) {
+            // set the owning side to null (unless already changed)
+            if ($avi->getUser() === $this) {
+                $avi->setUser(null);
+            }
+        }
+
+        return $this;
+    }
 
 }
